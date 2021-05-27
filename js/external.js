6b1b26bb4018f2433e756df1f1f8732df49730ef
Angela Sipes <angela.m.sipes73@gmail.com>
2021-05-27 13:35:54 -0500

    JavaScriptHTML

--- js/external.js
+++ js/external.js
@@ -1,1 +1,12 @@
+"use strict";
+//... rest of javascript code
 
+console.log("Hello World!");
+console.log("Hello Polaris");
+
+"hello students";
+
+alert("Hello World!");
+
+var userInput = prompt('Please type something:');
+console.log('The user entered: ' + userInput);
