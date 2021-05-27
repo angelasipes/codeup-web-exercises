eba6aeecac9c62729b2f04676b1391df746838fb
Angela Sipes <angela.m.sipes73@gmail.com>
2021-05-27 16:06:00 -0500

    To Do List

--- js/external.js
+++ js/external.js
@@ -4,9 +4,20 @@
 console.log("Hello World!");
 console.log("Hello Polaris");
 
-"hello students";
+console.log("hello students");
+
 
 alert("Hello World!");
 
 var userInput = prompt('Please type something:');
 console.log('The user entered: ' + userInput);
+alert("Welcome to my Website");
+
+var favoriteColor = prompt ("What is your favorite color?");
+alert("Great, " + favoriteColor + " is my favorite color too!");
+
+alert('The price for the movie is: (3+5+1)* 3)');
+
+
+
+
